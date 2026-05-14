8c093cfc59c66eb1fea6b77a925f23466367dbcd
Kajol P <kajlir192@gmail.com>
2026-05-13 18:21:26 -0700

    Added slug level winloss, textTransition(), and sceneTransition()

--- transitions.js
+++ transitions.js
@@ -1,3 +1,24 @@
+//to be added into the scene file, for transitions between levels and win screen
+
 slugLevelWin() {
-    this.add.text('You win!');
+    this.newMessage = this.add.text("That's good work!");
+    textTransition();
+}
+
+slugLevelLoss() {
+    this.newMessage = this.add.text("Watch out for snails!");
+    textTransition();
+}
+
+textTransition() {
+    this.tweens.add({
+            targets: this.newMessage,
+            alpha: { from: 1, to: 0 },
+            easing: 'Quintic.in',
+            duration: 3000        
+        });
+}
+
+sceneTransition() {
+    this.cameras.main.fade(1000, 0, 0, 0);
 }
